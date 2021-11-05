a2500033c1c2841462491761a0d71d2488e74d14
Michael <mjpj@live.com>
2021-11-04 23:37:55 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution here
+let findMatching = (array, string) =>array.filter( (possibleMatch) => possibleMatch.toLowerCase() === string.toLowerCase())
+
+let fuzzyMatch =(magic, string) => magic.filter( possibleMatch =>possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0)
+
+let matchName = (placeHolder, theName) => placeHolder.filter( record => record.name === theName)
